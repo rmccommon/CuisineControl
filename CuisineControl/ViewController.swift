57cc2097bb35c5f1f4be2d20dae23013329d0eed
Taylor McLaughlin <webstermclaughlin@hotmail.com>
2018-10-08 11:30:41 -0700

    Initial Commit

---
+++ CuisineControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CuisineControl
+//
+//  Created by Taylor McLaughlin on 10/8/18.
+//  Copyright © 2018 Taylor McLaughlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
